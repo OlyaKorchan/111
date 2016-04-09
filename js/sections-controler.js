19b0c80afd0c2f9a4188ad0fcee65995a20b951c
olyakorchan <korchanola@gmail.com>
2016-04-09 19:42:20 +0000

    added picture viewer

--- js/sections-controler.js
+++ js/sections-controler.js
@@ -7,13 +7,13 @@ $(function () {
 
     $("#menu-about, #menu-education, #menu-skills, #menu-contacts, #menu-interests").click(function (event) {
     	var contentID = event.target.id.replace("menu-", "#");
-        changeContentSectionTo(contentID, 1000);
+        changeContentSectionTo(contentID, 700);
     });
 
     function changeContentSectionTo(sectionName, animationDuration) {
         if (currentSection !== sectionName) {
         $("#content").children().hide();
-        $(sectionName).show(animationDuration ? animationDuration : 0);
+        $(sectionName).show(animationDuration ? animationDuration : 0, "linear");
         currentSection=sectionName;
     	 }
     }
