181f72489d735354f2eaa79e241e61b4262fc01a
olyakorchan <korchanola@gmail.com>
2016-04-10 19:39:56 +0000

    responsive for tablets

--- js/pictures-viewer.js
+++ js/pictures-viewer.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(function() {
 	$(".fancybox").fancybox({
 		openEffect	: 'none',
 		closeEffect	: 'none'
--- js/sections-controler.js
+++ js/sections-controler.js
@@ -1,20 +1,20 @@
-$(function () {
+$(function() {
     'use strict';
     console.log("ready!");
     var currentSection = "";
 
     changeContentSectionTo("#about");
 
-    $("#menu-about, #menu-education, #menu-skills, #menu-contacts, #menu-interests").click(function (event) {
-    	var contentID = event.target.id.replace("menu-", "#");
+    $("#menu-about, #menu-education, #menu-skills, #menu-contacts, #menu-interests").click(function(event) {
+        var contentID = event.target.id.replace("menu-", "#");
         changeContentSectionTo(contentID, 700);
     });
 
     function changeContentSectionTo(sectionName, animationDuration) {
         if (currentSection !== sectionName) {
-        $("#content").children().hide();
-        $(sectionName).show(animationDuration ? animationDuration : 0, "linear");
-        currentSection=sectionName;
-    	 }
+            $("#content").children().hide();
+            $(sectionName).show(animationDuration ? animationDuration : 0, "linear");
+            currentSection = sectionName;
+        }
     }
 });
